11cf8970c4e9cf59d6381ada20f07b0af5edb709
rakeenzaman <rakeenzaman@gmail.com>
2024-09-29 23:19:24 -0500

    final changes probably

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbModal, NgbModalRef, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import confetti from 'canvas-confetti';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -13,26 +14,26 @@ import confetti from 'canvas-confetti';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements AfterViewInit {
+  yay = new Audio();
+  goofyahh = new Audio();
+  names: string[] = [];
+  constructor(private ngbModal: NgbModal) {
+    // initializing audio files
+    this.yay.src = "../assets/yay.mp3";
+    this.goofyahh.src = "../assets/goofyahh.mp3";
+    this.yay.volume = 0.2;
+    this.goofyahh.volume = 0.1;
+    this.yay.load();
+    this.goofyahh.load();
 
-  constructor(private ngbModal: NgbModal) {}
+    // get names from local storage
+    this.names = JSON.parse(localStorage.getItem('spin-the-wheel-names') as string) || [];
+  }
 
   @ViewChild('popup') public popup!: TemplateRef<string>;
   @ViewChild('customBackgroundModal') public customBackgroundModal!: TemplateRef<string>;
 
   title = 'SpinTheWheel';
-
-  names = [
-    'test name 0',
-    'test name 1',
-    'test name 2',
-    'test name 3',
-    'test name 4',
-    'test name 5',
-    'test name 6',
-    'test name 7',
-    'test name 8',
-    'test name 9',
-  ];
 
   div!: HTMLElement;
   textbox: string = '';
@@ -40,6 +41,7 @@ export class AppComponent implements AfterViewInit {
   customBackgroundModalRef: NgbModalRef | null = null;
 
   chosenItem: string = '';
+  chosenItemIndex!: number;
   
   customBackground: string = '';
   customUrlTextBoxValue: string = '';
@@ -49,10 +51,11 @@ export class AppComponent implements AfterViewInit {
   }
 
   spin(): void {
+    setTimeout(() => this.goofyahh.play(), 400);
     this.div.style.transition = `transform 2s ease-in-out`;
-    const randomPick = this.generateRandomNumber(0, this.names.length - 1);
-    this.chosenItem = this.names[randomPick];
-    let x = 1440 - (randomPick/(this.names.length) * 360 + this.getRandomPxOffset());
+    this.chosenItemIndex = this.generateRandomNumber(0, this.names.length - 1);
+    this.chosenItem = this.names[this.chosenItemIndex];
+    let x = 1440 - (this.chosenItemIndex/(this.names.length) * 360 + this.getRandomPxOffset());
     this.div.style.transform = `rotate(${x}deg)`;
     setTimeout(() => this.openPopup(), 2500);
   }
@@ -77,16 +80,29 @@ export class AppComponent implements AfterViewInit {
       this.names.push(this.textbox);
       this.textbox = '';
     }
+    this.updateListInLocalStorage();
   }
 
   openPopup() {
+    this.yay.play();
     confetti({
       particleCount: 300,
       spread: 100,
       zIndex: 9999,
     });
-    this.popupRef = this.ngbModal.open(this.popup, { centered: true, windowClass: 'custom-modal' });
-    setTimeout(() => this.reset(), 1000);
+    this.popupRef = this.ngbModal.open(this.popup, { centered: true, windowClass: 'selected-popup', keyboard: false, backdrop : 'static', });
+  }
+
+  closePopup() {
+    this.popupRef?.close();
+    this.reset();
+  }
+
+  closePopupAndRemoveItem() {
+    this.popupRef?.close();
+    this.reset();
+    this.names.splice(this.chosenItemIndex, 1);
+    this.updateListInLocalStorage();
   }
 
   openCustomBackgroundModal() {
@@ -95,6 +111,24 @@ export class AppComponent implements AfterViewInit {
     } else {
       this.customBackgroundModalRef = this.ngbModal.open(this.customBackgroundModal, { centered: true, windowClass: 'custom-background-modal' });
     }
+  }
+
+  updateListInLocalStorage() {
+    localStorage.setItem('spin-the-wheel-names', JSON.stringify(this.names));
+  }
+
+  clearListInLocalStorage() {
+    localStorage.removeItem('spin-the-wheel-names');
+  }
+
+  clearList() {
+    this.names = [];
+    this.clearListInLocalStorage();
+  }
+
+  removeItem(index: number) {
+    this.names.splice(index, 1);
+    this.updateListInLocalStorage();
   }
 
   setCustomBackground() {
